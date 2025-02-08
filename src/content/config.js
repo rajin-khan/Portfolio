28c78f6f4195f769b830ddac95b7c90c369e8c3d
Rajin Khan <rajin.khan2001@gmail.com>
2025-02-08 20:19:13 +0600

    Update for deployment fixes

--- src/content/config.js
+++ src/content/config.js
@@ -1,14 +1,15 @@
 import { defineCollection, z } from "astro:content";
 
 const postCollection = defineCollection({
-	type: "content",
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		dateFormatted: z.string(),
-	}),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.date(),
+    slug: z.string(),
+    tags: z.array(z.string()).optional(),
+  }),
 });
 
 export const collections = {
-	post: postCollection,
+  post: postCollection,
 };
