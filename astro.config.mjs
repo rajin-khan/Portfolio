5ffc2f05d74f86cc080dc2e266e9d2c80ba62bb9
Rajin Khan <rajin.khan2001@gmail.com>
2025-05-24 20:55:15 +0600

    Build sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'http://rajinkhan.vercel.app',
-	integrations: [tailwind()],
+    site: 'https://rajinkhan.com',
+    integrations: [tailwind(), sitemap()],
 });
