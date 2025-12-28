d256f55e7f431163bd11354b381b87f53b11f59c
Rajin Khan <rajin.khan2001@gmail.com>
2025-12-29 01:11:15 +0600

    Configure Vercel to use Node.js 20 runtime

--- .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_CxwLsURe.mjs';
-import { manifest } from './manifest_C5Fhlew2.mjs';
+import { manifest } from './manifest_CFjG-3ix.mjs';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
 const _page1 = () => import('./pages/404.astro.mjs');
@@ -63,7 +63,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "5cbb6c60-5184-4bfc-92ee-f2f2485a5d4c",
+    "middlewareSecret": "82a9023a-389b-42f9-9039-24f46e6156a1",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
